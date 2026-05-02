ae70c62e336435ad20c5b65946fc0a8a25471581
Cmunoz <cmunozm6@miumg.edu.gt>
2026-05-01 20:52:47 -0600

    encabezado con navegacion responsive

--- resources/views/admin/viajes/index.blade.php
+++ resources/views/admin/viajes/index.blade.php
@@ -34,5 +34,65 @@
     </div>
 </div>
 
+<header>
+    <div class="header-area">
+        <div class="main-header">
+            <div class="header-top d-none d-lg-block">
+                <div class="container">
+                    <div class="col-xl-12">
+                        <div class="row d-flex justify-content-between align-items-center">
+                            <div class="header-info-left">
+                                <ul>
+                                    <li>Numero: +99 (0) 101 0000 888</li>
+                                    <li>Email: noreply@yourdomain.com</li>
+                                </ul>
+                            </div>
+                            <div class="header-info-right">
+                                <ul class="header-social">
+                                    <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-google-plus-g"></i></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="header-bottom header-sticky">
+                <div class="container">
+                    <div class="row align-items-center">
+                        <div class="col-xl-2 col-lg-2">
+                            <div class="logo">
+                                <a href="index.html">
+                                    <img src="assets/img/logo/logoNombre.png" alt="">
+                                </a>
+                            </div>
+                        </div>
+                        <div class="col-xl-10 col-lg-10">
+                            <div class="menu-wrapper d-flex align-items-center justify-content-end">
+                                <div class="main-menu d-none d-lg-block">
+                                    <nav>
+                                        <ul id="navigation" class="d-flex align-items-center">
+                                            <li><a href="/">Inicio</a></li>
+                                            <li><a href="#">Contactanos</a></li>
+                                        </ul>
+                                    </nav>
+                                </div>
+                                <div class="header-right-btn d-none d-lg-block ml-20">
+                                    <a href="contact.html" class="btn header-btn">Obten tu cotización</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-12">
+                            <div class="mobile_menu d-block d-lg-none"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
+
 </body>
 </html>
