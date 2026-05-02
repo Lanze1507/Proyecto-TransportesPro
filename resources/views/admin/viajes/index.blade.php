11ecb669053bb7eb01da98c2d22d09a035f31386
Cmunoz <cmunozm6@miumg.edu.gt>
2026-05-01 20:51:25 -0600

    se agrega animacion para precarga de la pagina

--- resources/views/admin/viajes/index.blade.php
+++ resources/views/admin/viajes/index.blade.php
@@ -22,5 +22,17 @@
 </head>
 <body>
 
+<!-- Preloader -->
+<div id="preloader-active">
+    <div class="preloader d-flex align-items-center justify-content-center">
+        <div class="preloader-inner position-relative">
+            <div class="preloader-circle"></div>
+            <div class="preloader-img pere-text">
+                <img src="{{ asset('assets/img/logo/loder.jpg') }}" alt="">
+            </div>
+        </div>
+    </div>
+</div>
+
 </body>
 </html>
