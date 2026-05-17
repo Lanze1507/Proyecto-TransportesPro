5fe33a825085b7b93111ba684fdfdd814f2b7d73
Cmunoz <cmunozm6@miumg.edu.gt>
2026-05-17 18:56:00 -0500

    se crea el dashboard para pilotos

--- backup.sql
+++ backup.sql
@@ -1,4 +1,4 @@
--- MySQL dump 10.13  Distrib 8.0.45, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.4.3, for Win64 (x86_64)
 --
 -- Host: localhost    Database: transpro
 -- ------------------------------------------------------
